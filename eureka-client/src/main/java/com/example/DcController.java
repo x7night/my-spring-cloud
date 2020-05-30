d784fc37a2fdfea45dc6ea1eb102079ed2087dfc
destiny.X <565306878@qq.com>
2020-05-30 16:02:53 +0800

    add hystrix

--- eureka-client/src/main/java/com/example/DcController.java
+++ eureka-client/src/main/java/com/example/DcController.java
@@ -12,7 +12,8 @@ public class DcController {
     private DiscoveryClient discoveryClient;
 
     @GetMapping("/dc")
-    public String dc() {
+    public String dc() throws InterruptedException {
+        Thread.sleep(5000L);
         String services = "Services: " + discoveryClient.getServices();
         System.out.println(services);
         return services;
---
+++ eureka-consumer-ribbon-hystrix/src/main/java/com/example/eurekaconsumerribbonhystrix/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.eurekaconsumerribbonhystrix;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(DemoApplication.class).web(true).run(args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
