95f4014473e53c6a93c2308b0a74c7a6009bf380
destiny.X <565306878@qq.com>
2020-05-31 14:38:41 +0800

    back to normal

--- eureka-client/src/main/java/com/example/DcController.java
+++ eureka-client/src/main/java/com/example/DcController.java
@@ -13,7 +13,7 @@ public class DcController {
 
     @GetMapping("/dc")
     public String dc() throws InterruptedException {
-        Thread.sleep(5000L);
+        // Thread.sleep(5000L);
         String services = "Services: " + discoveryClient.getServices();
         System.out.println(services);
         return services;
